1513d4c755f919e926599ebc3bf169fe3bfbb3a6
Mihail Marian <mihailmarian12@gmail.com>
2021-11-15 08:26:07 +0000

    added links

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function FirstPost() {
-    return <h1>First Post</h1>
+  return (
+    <>
+      {" "}
+      <h1>First Post</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
 }
